911834eb79f3ae4f5146ba9db38c027939c720fd
Алексей <akarpushev@yandex.ru>
2024-08-04 23:03:22 +0300

    admin.py, serializers.py, views.py

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from .models import Employee
+from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ['id', 'first_name', 'last_name', 'email']
+        extra_kwargs = {
+            'email': {'required': True, 'validators': [EmailValidator()]}}
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import Employee
-from .serializers import EmployeeSerializer
+from .serializers import EmployeeSerializer, UserSerializer, LoginSerializer
 from django.http import HttpResponse
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework import status
@@ -25,22 +24,22 @@ class EmployeeDetailView(generics.RetrieveAPIView):
 
 class RegisterUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
-    serializer_class = EmployeeSerializer
+    serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        if User.objects.filter(username=username).exists():
-            return Response({'error': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        user = User.objects.create_user(username=username, password=password)
-        user.save()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
 
 
 class LoginUserView(generics.GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
